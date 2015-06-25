0d15826591d2b78a56b56eeb3235802cc2fa7b1b
Maamar Yacine MEDDAH <my.meddah@gmail.com>
2015-06-25 15:29:08 +0200

    user doc + bootstrapping

--- API/CHECK_SESSION/index.php
+++ API/CHECK_SESSION/index.php
@@ -2,7 +2,7 @@
 	if (isset($_GET['user'])){
 		session_start();
 		if (isset($_SESSION['user'])){
-			if ($_GET['user'] == $_SESSION['user']) echo $_SESSION['user'];
+			if ($_GET['user'] == $_SESSION['user']) echo $_GET['user'];
 		}
 	}
 ?>
--- js/services.js
+++ js/services.js
@@ -6,13 +6,21 @@ app.factory('sessionService', function($http){
     var API_URI = 'api/';
     return {
         set:function(key, value){
+            /**
+             * Ajoute des variable de session clé, valeur
+             */
             return sessionStorage.setItem(key, value);
         },
         get:function(key){
+            /**
+             * Permet de recupperer la valeur d'une variable de session par sa clé
+             */
             return sessionStorage.getItem(key);
         },
         destroy:function(){
-            $http.post('API/DESTROY/');// TODO : déplacer cela dans la boucle car la je supprime uniquement 'user'
+            /**
+             * Ici je détruis toutes les variables de  session coté client
+             */
             for (var key in sessionStorage){
                 sessionStorage.removeItem(key, sessionStorage.getItem[key]);
             }
@@ -23,7 +31,7 @@ app.factory('sessionService', function($http){
 app.factory('loginService', function($http, $location, sessionService){
    return{
        login:function(data, $scope){
-           var $promise = $http.post('API/',data);
+           var $promise = $http.post('API/',data); //  vous pouvez passer par le success et error de $http
            $promise.then(function(msg){
                 var userId = msg.data;
                if (userId!="fail"){
@@ -36,10 +44,16 @@ app.factory('loginService', function($http, $location, sessionService){
            });
        },
        logout: function(){
-           sessionService.destroy();
-           $location.path('/login');
+           sessionService.destroy(); //destruction de la session coté client
+           $http.post('API/DESTROY/');//destruction de la session coté serveur
+           $location.path('/login'); // redirection vers la page du login
        },
        isLogged: function(){
+           /**
+            * Pour plus de sécurité j'envoie  la variable de session user à mon serveur qui devrait avoir la même
+            * Si ce n'est pas le cas le serveur ne renvoie rien et la redirection est faite sur authentificationApp
+            * (app.js line 18) via le logout qui détruit la session client et de même du côté du serveur
+            */
            var user = sessionService.get('user');
            var  $checkSessionServer = $http.get('API/CHECK_SESSION/?user='+user);
            return $checkSessionServer;
